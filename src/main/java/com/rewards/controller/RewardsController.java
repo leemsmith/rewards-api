b546082b74d1b987df712e52328aef2143d981ea
Samuel Vandersaul <samuel@vandersaul.com>
2026-08-29 16:23:35 -0500

    Added service layer

--- src/main/java/com/rewards/controller/RewardsController.java
+++ src/main/java/com/rewards/controller/RewardsController.java
@@ -1,13 +1,21 @@
 package com.rewards.controller;
 
+import com.rewards.service.RewardsService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RewardsController {
 
+    private final RewardsService rewardsService;
+
+    public RewardsController(RewardsService rewardsService) {
+        this.rewardsService = rewardsService;
+    }
+
     @GetMapping("/rewards")
     public String getRewards() {
-        return "Rewards points";
+        Integer rewards = rewardsService.calculateRewards();
+        return "Rewards points: " + rewards;
     }
 }
---
+++ src/main/java/com/rewards/service/RewardsService.java
@@ -0,0 +1,14 @@
+package com.rewards.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RewardsService {
+
+    public RewardsService() {
+    }
+
+    public Integer calculateRewards() {
+        return 0;
+    }
+}
